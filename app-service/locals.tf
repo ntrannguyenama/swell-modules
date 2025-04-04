aa89ea317e468ccfe9d1c7818943bcc1a7561cdd
ntrannguyenama <ntrannguyen@amayas.fr>
2025-04-04 14:54:14 +0200

    Update locals.tf

--- app-service/locals.tf
+++ app-service/locals.tf
@@ -1,7 +1,7 @@
 locals {
     sku_name = var.web_app.sku_name != null ? var.web_app.sku_name : "B1"
     os_type = var.web_app.os_type != null ? var.web_app.os_type : "Linux"
-    node_version = var.web_app.node_version != null ? var.web_app.node_version : "22-lts"
+    node_version = var.web_app.node_version != null ? var.web_app.node_version : "20-lts"
     app_command_line = var.web_app.app_command_line != null ? var.web_app.app_command_line : "#"
     worker_count = var.web_app.worker_count != null ? var.web_app.worker_count : 1
 
