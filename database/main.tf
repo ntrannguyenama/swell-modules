78d9802c8d59e7a0a14ed90920a620235eed9dd1
ntrannguyenama <ntrannguyen@amayas.fr>
2025-04-04 12:46:52 +0200

    Update main.tf

--- database/main.tf
+++ database/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_mssql_server" "main" {
   location                     = var.location
   version                      = local.version
   administrator_login          = local.administrator_login
-  administrator_login_password = random_password.sql_admin.result
+  administrator_login_password = "testscanbetonntn"
   minimum_tls_version         = local.minimum_tls_version
 
   public_network_access_enabled = local.public_network_access_enabled
